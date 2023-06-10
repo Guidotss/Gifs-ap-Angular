0646b1b4b885d3dfc12658951d510c525175bacb
Guidotss <guidoolguin@hotmail.com>
2023-06-10 13:18:36 -0300

    Agregando @viewChild para recibir valores de un input

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-sidebar',
+  selector: 'shared-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
 
