1091945e85317c7d26437a2359edd66233f0170d
Guidotss <guidoolguin@hotmail.com>
2023-06-16 21:40:22 -0300

    Terminando lazyImage component

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html',
 })
 
-export class LazyImageComponent {
+export class LazyImageComponent implements OnInit{
+  @Input()
+  public url:string = '';
 
+  @Input()
+  public alt:string = '';
+
+  private _hasLoaded:boolean = false;
+
+
+  ngOnInit(): void {
+    if(!this.url)
+    throw new Error('Url property is required');
+  }
+
+  public onLoad():void{
+    console.log('Image has loaded');
+    this._hasLoaded = true;
+  }
+
+  public get hasLoaded():boolean{
+    return this._hasLoaded;
+  }
 }
